7cd30d6e9b7e7ec816febd285e698eb60850d313
vtu29232-rgb <vtu29232@veltech.edu.in>
2026-09-08 20:33:20 +0530

    Delete task9 sum of last digits of two numbers.java

--- task9 sum of last digits of two numbers.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Task9{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println((a%10)+(b%10));
-    }
